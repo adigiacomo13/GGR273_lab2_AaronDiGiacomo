9fcc103288a2d1edaf58e2ca1d7d564c72d01583
adigiacomo13 <aaron.digiacomo@mail.utoronto.ca>
2024-08-07 21:52:12 -0400

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,71 @@
+ol.proj.proj4.register(proj4);
+//ol.proj.get("EPSG:3403").setExtent([-2473172.943476, 5235956.269612, 2595738.383208, 6884158.136054]);
+var wms_layers = [];
+
+var format_AlbertaBoundary_0 = new ol.format.GeoJSON();
+var features_AlbertaBoundary_0 = format_AlbertaBoundary_0.readFeatures(json_AlbertaBoundary_0, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3403'});
+var jsonSource_AlbertaBoundary_0 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_AlbertaBoundary_0.addFeatures(features_AlbertaBoundary_0);
+var lyr_AlbertaBoundary_0 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_AlbertaBoundary_0, 
+                style: style_AlbertaBoundary_0,
+                popuplayertitle: "Alberta Boundary",
+                interactive: false,
+                title: '<img src="styles/legend/AlbertaBoundary_0.png" /> Alberta Boundary'
+            });
+var format_AlbertaWaterbodies_1 = new ol.format.GeoJSON();
+var features_AlbertaWaterbodies_1 = format_AlbertaWaterbodies_1.readFeatures(json_AlbertaWaterbodies_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3403'});
+var jsonSource_AlbertaWaterbodies_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_AlbertaWaterbodies_1.addFeatures(features_AlbertaWaterbodies_1);
+var lyr_AlbertaWaterbodies_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_AlbertaWaterbodies_1, 
+                style: style_AlbertaWaterbodies_1,
+                popuplayertitle: "Alberta Waterbodies",
+                interactive: false,
+                title: '<img src="styles/legend/AlbertaWaterbodies_1.png" /> Alberta Waterbodies'
+            });
+var format_FishStocking2024_2 = new ol.format.GeoJSON();
+var features_FishStocking2024_2 = format_FishStocking2024_2.readFeatures(json_FishStocking2024_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3403'});
+var jsonSource_FishStocking2024_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_FishStocking2024_2.addFeatures(features_FishStocking2024_2);
+var lyr_FishStocking2024_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_FishStocking2024_2, 
+                style: style_FishStocking2024_2,
+                popuplayertitle: "Fish Stocking (2024)",
+                interactive: true,
+    title: 'Fish Stocking (2024)<br />\
+    <img src="styles/legend/FishStocking2024_2_0.png" /> 0 - 1807<br />\
+    <img src="styles/legend/FishStocking2024_2_1.png" /> 1807 - 5600<br />\
+    <img src="styles/legend/FishStocking2024_2_2.png" /> 5600 - 11500<br />\
+    <img src="styles/legend/FishStocking2024_2_3.png" /> 11500 - 20000<br />\
+    <img src="styles/legend/FishStocking2024_2_4.png" /> 20000 - 40000<br />\
+    <img src="styles/legend/FishStocking2024_2_5.png" /> 40000 - 110000<br />\
+    <img src="styles/legend/FishStocking2024_2_6.png" /> 110000 - 1613075<br />'
+        });
+
+lyr_AlbertaBoundary_0.setVisible(true);lyr_AlbertaWaterbodies_1.setVisible(true);lyr_FishStocking2024_2.setVisible(true);
+var layersList = [lyr_AlbertaBoundary_0,lyr_AlbertaWaterbodies_1,lyr_FishStocking2024_2];
+lyr_AlbertaBoundary_0.set('fieldAliases', {'PRUID': 'PRUID', 'DGUID': 'DGUID', 'PRNAME': 'PRNAME', 'PRENAME': 'PRENAME', 'PRFNAME': 'PRFNAME', 'PREABBR': 'PREABBR', 'PRFABBR': 'PRFABBR', 'LANDAREA': 'LANDAREA', });
+lyr_AlbertaWaterbodies_1.set('fieldAliases', {'HYDROUID': 'HYDROUID', 'NAME': 'NAME', 'RANK': 'RANK', 'PRUID': 'PRUID', });
+lyr_FishStocking2024_2.set('fieldAliases', {'DISTRICT': 'DISTRICT', 'WATERBODY': 'WATERBODY', 'Latitude': 'Latitude', 'Longitude': 'Longitude', 'SPECIES': 'SPECIES', 'GENOTYPE': 'GENOTYPE', 'STOCKED -': 'STOCKED -', 'STOCKING N': 'STOCKING N', 'PLANNED ST': 'PLANNED ST', 'Map Author': 'Map Author', 'NumStock': 'NumStock', });
+lyr_AlbertaBoundary_0.set('fieldImages', {'PRUID': 'TextEdit', 'DGUID': 'TextEdit', 'PRNAME': 'TextEdit', 'PRENAME': 'TextEdit', 'PRFNAME': 'TextEdit', 'PREABBR': 'TextEdit', 'PRFABBR': 'TextEdit', 'LANDAREA': 'TextEdit', });
+lyr_AlbertaWaterbodies_1.set('fieldImages', {'HYDROUID': 'TextEdit', 'NAME': 'TextEdit', 'RANK': 'Range', 'PRUID': 'TextEdit', });
+lyr_FishStocking2024_2.set('fieldImages', {'DISTRICT': 'TextEdit', 'WATERBODY': 'TextEdit', 'Latitude': 'TextEdit', 'Longitude': 'TextEdit', 'SPECIES': 'TextEdit', 'GENOTYPE': 'TextEdit', 'STOCKED -': 'TextEdit', 'STOCKING N': 'TextEdit', 'PLANNED ST': 'TextEdit', 'Map Author': 'TextEdit', 'NumStock': 'Range', });
+lyr_AlbertaBoundary_0.set('fieldLabels', {'PRUID': 'no label', 'DGUID': 'no label', 'PRNAME': 'no label', 'PRENAME': 'no label', 'PRFNAME': 'no label', 'PREABBR': 'no label', 'PRFABBR': 'no label', 'LANDAREA': 'no label', });
+lyr_AlbertaWaterbodies_1.set('fieldLabels', {'HYDROUID': 'no label', 'NAME': 'no label', 'RANK': 'no label', 'PRUID': 'no label', });
+lyr_FishStocking2024_2.set('fieldLabels', {'DISTRICT': 'inline label - visible with data', 'WATERBODY': 'inline label - visible with data', 'Latitude': 'hidden field', 'Longitude': 'hidden field', 'SPECIES': 'inline label - visible with data', 'GENOTYPE': 'inline label - visible with data', 'STOCKED -': 'hidden field', 'STOCKING N': 'hidden field', 'PLANNED ST': 'inline label - visible with data', 'Map Author': 'header label - always visible', 'NumStock': 'inline label - always visible', });
+lyr_FishStocking2024_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/FishStocking2024_2_style.js
@@ -0,0 +1,82 @@
+var size = 0;
+var placement = 'point';
+
+var style_FishStocking2024_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("NumStock");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 0.000000 && value <= 1807.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(255,245,235,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 1807.000000 && value <= 5600.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(254,224,194,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 5600.000000 && value <= 11500.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(253,189,131,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 11500.000000 && value <= 20000.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(253,146,67,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 20000.000000 && value <= 40000.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(238,101,17,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 40000.000000 && value <= 110000.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(194,64,2,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 110000.000000 && value <= 1613075.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(127,39,4,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
